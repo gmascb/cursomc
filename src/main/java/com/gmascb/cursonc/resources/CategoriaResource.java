2bc976d955784707a6f2f32cb1c271fe1f04d818
Guilherme Mascarenhas <gmascb@gmail.com>
2018-08-11 18:19:02 -0300

    PrimeiraClasseDeDominio

--- src/main/java/com/gmascb/cursonc/resources/CategoriaResource.java
+++ src/main/java/com/gmascb/cursonc/resources/CategoriaResource.java
@@ -1,17 +1,29 @@
 package com.gmascb.cursonc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gmascb.cursonc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar()
+	public List<Categoria> listar()
 	{
-		return "Rest esta funcionando!";
+		Categoria cat = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lstCategoria = new ArrayList<Categoria>();
+		lstCategoria.add(cat);
+		lstCategoria.add(cat2);
+		
+		return lstCategoria;
 	}
 	
 }
