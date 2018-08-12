854694a74d41461cf850e84a12a369410ff1490e
Guilherme Mascarenhas <gmascb@gmail.com>
2018-08-11 23:57:28 -0300

    Adicionando run na app

--- src/main/java/com/gmascb/cursonc/CursomcApplication.java
+++ src/main/java/com/gmascb/cursonc/CursomcApplication.java
@@ -1,12 +1,30 @@
 package com.gmascb.cursonc;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.gmascb.cursonc.domain.Categoria;
+import com.gmascb.cursonc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	private CategoriaRepository catRep;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		catRep.saveAll(Arrays.asList(cat1, cat2));
+	    
 	}
 }
